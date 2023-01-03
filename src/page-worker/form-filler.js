2ee73110f8a89785a0d531074cb919960b46726a
Ivan Chelishchev <chelishchev@gmail.com>
2023-01-03 20:13:58 +0200

    Делаем автоматическое заполнение полей, когда они появляются.

--- src/page-worker/form-filler.js
+++ src/page-worker/form-filler.js
@@ -1,4 +1,10 @@
+const NODE_IDS = {
+    ID_KEYPAD: 'ID_KEYPAD',
+    PHONE_KEYPAD: 'PHONE_KEYPAD',
+    mobileNumber: 'mobileNumber',
+}
 export class FormFiller {
+
     constructor() {
     }
 
@@ -6,10 +12,46 @@ export class FormFiller {
         this.fillIDTeudat(person.idNumber);
         this.fillPhoneShort(person.shortMobilePhone);
         this.fillMobileNumber(person.phoneNumber);
+
+        this.registerAutoFiller(person);
     }
 
+    registerAutoFiller(person) {
+        const processFill = this.debounce(() => {
+            this.fillIDTeudat(person.idNumber);
+            this.fillPhoneShort(person.shortMobilePhone);
+            this.fillMobileNumber(person.phoneNumber);
+        }, 250);
+
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                if (mutation.type === 'attributes') {
+                    const nodeId = mutation.target.id;
+                    if (NODE_IDS.hasOwnProperty(nodeId)) {
+                        processFill();
+                    }
+                }
+            });
+        });
+
+        observer.observe(document.body, {childList: true, subtree: true, attributes: true,});
+    }
+
+    debounce(fn, delay) {
+        let timer = null;
+        return function () {
+            const context = this;
+            const args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                fn.apply(context, args);
+            }, delay);
+        }
+    }
+
+
     fillIDTeudat(idNumber) {
-        const idTeudat = document.querySelector('#ID_KEYPAD');
+        const idTeudat = document.querySelector(`#${NODE_IDS.ID_KEYPAD}`);
         if (idTeudat) {
             this.setValueAndDispatchEvent(idTeudat, idNumber);
             this.submitFormAfterDelay('form[name="questionnaireForm"] button', 100);
@@ -17,7 +59,7 @@ export class FormFiller {
     }
 
     fillPhoneShort(shortMobilePhone) {
-        const phoneShort = document.querySelector('#PHONE_KEYPAD');
+        const phoneShort = document.querySelector(`#${NODE_IDS.PHONE_KEYPAD}`);
         if (phoneShort) {
             this.setValueAndDispatchEvent(phoneShort, shortMobilePhone);
             this.submitFormAfterDelay('form[name="questionnaireForm"] button', 100);
@@ -25,7 +67,7 @@ export class FormFiller {
     }
 
     fillMobileNumber(phoneNumber) {
-        const mobileNumber = document.querySelector('#mobileNumber');
+        const mobileNumber = document.querySelector(`#${NODE_IDS.mobileNumber}`);
         if (mobileNumber) {
             this.setValueAndDispatchEvent(mobileNumber, phoneNumber);
             this.submitFormAfterDelay('form[name="smsSettingsForm"] input[type="submit"]', 100);
