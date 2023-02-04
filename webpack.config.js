181a7a00f00cdb66b1f9aaeb21c8d39c6ce9ab1f
Ivan Chelishchev <chelishchev@gmail.com>
2023-02-04 19:17:33 +0200

    Убираем popup пока. Так как в нём хз, что должно быть в текущем состоянии

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
 	entry: {
 		options: './src/options/index.js',
-		popup: './src/popup/index.js',
 		'page-worker': './src/page-worker/index.js',
 	},
 	output: {
